fd22b6c82a526de273e96b35e0ebfeb06d8b4289
Tanaka Edgar-TANAKAED <tanakaed@motorola.com>
2018-04-15 15:36:23 -0300

    ex.2 separated into two programs

--- ex2/encode.py
+++ ex2/encode.py
@@ -31,38 +31,53 @@ def write_bit(_byte, bit, i):
 
 
 def read_file(input_file):
+    '''
+    Reads text file as a string
+    :param input_file: text file
+    :return: String inside text file
+    '''
     with open(input_file, 'r') as myfile:
         data = myfile.read().replace('\n', '')
         return data
 
 
-def write_message(input_image_file, output_image_file, message, bit_plane=0):
+def write_message(input_image_file, output_image_file, message_file, bit_plane=0):
     '''
     bit_plane: 0 is the least significant, 1 is the 2nd least significant and so on...
     '''
+
+    # load original pristine image
     img = io.imread(input_image_file)
     original_shape = img.shape
     img = img.flatten()
-    message = message + '\0'
-    ascii_bytes = [ord(c) for c in message]
 
+    # read string message from file
+    message_string = read_file(message_file)
+    message_string = message_string + '\0'
+    ascii_bytes = [ord(c) for c in message_string]
+
+    # check if image can fit this message string
+    bits_needed = len(message_string) * 8
+    assert bits_needed <= len(img), "This message is too large to be inserted into this image. Not enough pixels."
+
+    # loop through each character in the message
     img_index = 0
     for message_index, ascii_byte in enumerate(ascii_bytes):
+        # for each of the 8 bits in this character (ASCII allows for 8 bits for each character)
         for bit_index in range(7, -1, -1):
+            # write bit into image
             bit = get_bit(ascii_byte, bit_index)
-
             img[img_index] = write_bit(img[img_index], bit, bit_plane)
             img_index += 1
 
-    print("modified bits:", img_index)
-    print("image shape:", original_shape)
-    print("total pixels:", original_shape[0] * original_shape[1])
-    print("len:", len(img))
-    percentage_modified = (img_index)/len(img)
-    pixels_modified = math.ceil(img_index/3)
-    print("modified pixels:", pixels_modified)
-    print("modified % pixels:", percentage_modified)
+    # print some info
+    print("Image shape:", original_shape)
+    print("Total pixels:", original_shape[0] * original_shape[1])
+    print("# Modified bits:", img_index)
+    print("# Modified pixels:", math.ceil(img_index/3))
+    print("% Modified pixels:", (img_index)/len(img))
 
+    # save output image with message inside it
     io.imsave(output_image_file, img.reshape(original_shape))
 
 
@@ -70,7 +85,6 @@ def print_bit_planes(input_file, output_file, bit_plane=0):
     img = io.imread(input_file)
     mask = 2 ** bit_plane
     bit_plane_img = np.bitwise_and(img, mask)
-    #print(bit_plane_img[:10][:10])
     io.imsave(output_file, bit_plane_img)
 
 
@@ -81,12 +95,13 @@ def main():
     bit_plane = int(sys.argv[3])
     output_image_file = sys.argv[4]
 
-    bit_plane_file = 'bits.' + input_image_file
-    message_string = read_file(message_file)
+    # write message into image file
+    write_message(input_image_file, output_image_file, message_file, bit_plane)
 
-    write_message(input_image_file, output_image_file, message_string, bit_plane)
-
-    print_bit_planes(output_image_file, bit_plane_file, bit_plane)
+    # print bit plane images for analysis
+    for bit_plane in [0, 1, 2, 7]:
+        bit_plane_file = 'bits' + str(bit_plane) + '.' + input_image_file
+        print_bit_planes(output_image_file, bit_plane_file, bit_plane)
 
 
 if __name__ == "__main__":
--- ex2/run.sh
+++ ex2/run.sh
@@ -2,8 +2,8 @@ rm decoded.message*.txt
 rm bits*.png
 rm encoded*.png
 
-python3 encode.py baboon.png message_small.txt 2 encoded.baboon.png
-python3 encode.py monalisa.png message_small.txt 2 encoded.monalisa.png
+python3 encode.py baboon.png message_big.txt 2 encoded.baboon.png
+python3 encode.py monalisa.png message_big.txt 2 encoded.monalisa.png
 python3 encode.py peppers.png message_small.txt 2 encoded.peppers.png
 python3 encode.py watch.png message_small.txt 2 encoded.watch.png
 
