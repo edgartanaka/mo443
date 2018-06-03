133c970ccd2e71b1493fe45847406a393514485b
Tanaka Edgar-TANAKAED <tanakaed@motorola.com>
2018-06-03 18:04:43 -0300

    done working

---
+++ ex4/main.py
@@ -0,0 +1,240 @@
+#
+# Exercicio 4
+# MO443 - Processamento de Imagens
+# Prof. Helio Pedrini 1o. sem 2018
+# Aluno: Edgar Tanaka
+# RA: 023577
+#
+import matplotlib
+matplotlib.use('Agg')
+import argparse
+import math
+import matplotlib
+import numpy as np
+import sys
+from matplotlib import cm
+from matplotlib import pyplot as plt
+from scipy import misc
+from skimage import measure, io
+
+
+def calculate_dx_dy(indexes):
+    dx_dy = indexes - np.floor(indexes)
+    return dx_dy[0], dx_dy[1]
+
+
+def l_function(n, dx, dy, x, y, original_img):
+    width = original_img.shape[0] - 1
+    height = original_img.shape[1] - 1
+    y = np.clip(y + n - 2, 0, height)  # y + n - 2
+    l = (-dx * (dx - 1) * (dx - 2) * original_img[np.clip(x - 1, 0, width), y]) / 6
+    l += ((dx + 1) * (dx - 1) * (dx - 2) * original_img[x, y]) / 2
+    l += (-dx * (dx + 1) * (dx - 2) * original_img[np.clip(x + 1, 0, width), y]) / 2
+    l += (dx * (dx + 1) * (dx - 1) * original_img[np.clip(x + 2, 0, width), y]) / 6
+    return l
+
+
+def interpolate_lagrange(indexes, original_img):
+    dx, dy = calculate_dx_dy(indexes)
+    x = np.floor(indexes[0]).astype(int)
+    y = np.floor(indexes[1]).astype(int)
+    scaled_img = (-dy * (dy - 1) * (dy - 2) * l_function(1, dx, dy, x, y, original_img)) / 6
+    scaled_img += ((dy + 1) * (dy - 1) * (dy - 2) * l_function(2, dx, dy, x, y, original_img)) / 2
+    scaled_img += (-dy * (dy + 1) * (dy - 2) * l_function(3, dx, dy, x, y, original_img)) / 2
+    scaled_img += (dy * (dy + 1) * (dy - 1) * l_function(4, dx, dy, x, y, original_img)) / 6
+    return scaled_img.astype(int)
+
+
+def r_function(s):
+    return ((np.maximum(s + 2, 0) ** 3) - 4 * (np.maximum(s + 1, 0) ** 3) + 6 * (np.maximum(s, 0) ** 3) - 4 * (
+                np.maximum(s - 1, 0) ** 3)) / 6
+
+
+def interpolate_bicubic(indexes, original_img):
+    dx, dy = calculate_dx_dy(indexes)
+    scaled_img = np.zeros(dx.shape)
+    width = original_img.shape[0] - 1
+    height = original_img.shape[1] - 1
+
+    for m in range(-1, 3):
+        for n in range(-1, 3):
+            r2 = r_function(m - dx)
+            r1 = r_function(dy - n)
+            x = np.floor(indexes[0]).astype(int)
+            y = np.floor(indexes[1]).astype(int)
+            x = np.clip(x + m, 0, width)
+            y = np.clip(y + n, 0, height)
+            scaled_img += r1 * r2 * original_img[x, y]
+
+    return scaled_img.astype(int)
+
+
+def interpolate_bilinear(indexes, original_img):
+    width = original_img.shape[0] - 1
+    height = original_img.shape[1] - 1
+
+    dx, dy = calculate_dx_dy(indexes)
+    scaled_img = np.zeros(dx.shape)
+    x = np.floor(indexes[0]).astype(int)
+    y = np.floor(indexes[1]).astype(int)
+
+    scaled_img += (1 - dx) * (1 - dy) * original_img[np.clip(x, 0, width), np.clip(y, 0, height)]
+    scaled_img += dx * (1 - dy) * original_img[np.clip(x + 1, 0, width), np.clip(y, 0, height)]
+    scaled_img += (1 - dx) * dy * original_img[np.clip(x, 0, width), np.clip(y + 1, 0, height)]
+    scaled_img += dx * dy * original_img[np.clip(x + 1, 0, width), np.clip(y + 1, 0, height)]
+
+    return scaled_img.astype(int)
+
+
+def interpolate_nn(indexes, original_img):
+    indexes = np.round(indexes).astype(int)
+    width = original_img.shape[0] - 1
+    height = original_img.shape[1] - 1
+    x = indexes[0]
+    y = indexes[1]
+    return original_img[np.clip(x, 0, width), np.clip(y, 0, height)]
+
+
+def scale_image(method, original_img, scale):
+    scaled_shape = np.floor(np.multiply(original_img.shape, scale)).astype(int)
+    print("scaled shape:", scaled_shape)
+    scaled_img = np.zeros(scaled_shape)
+
+    # scale
+    indexes = np.indices(scaled_shape)
+    indexes = indexes / scale
+
+    if method == 4:
+        return interpolate_lagrange(indexes, original_img)
+    elif method == 3:
+        return interpolate_bicubic(indexes, original_img)
+    elif method == 2:
+        return interpolate_bilinear(indexes, original_img)
+    else:
+        return interpolate_nn(indexes, original_img)
+
+
+def rotate_image(method, original_img, theta):
+    '''
+    theta in degrees
+    '''
+    theta = np.deg2rad(theta)
+    indexes = np.indices(original_img.shape)
+
+    x = indexes[0]
+    y = indexes[1]
+    w = original_img.shape[0]
+    h = original_img.shape[1]
+    x_ = (x * math.cos(theta)) - (y * math.sin(theta))
+    y_ = (x * math.sin(theta)) + (y * math.cos(theta))
+
+    # calculate offset to center the image
+    center_x = ((w / 2) * math.cos(theta)) - ((h / 2) * math.sin(theta))
+    center_y = ((w / 2) * math.sin(theta)) + ((h / 2) * math.cos(theta))
+    offset_x = (w / 2) - center_x
+    offset_y = (h / 2) - center_y
+
+    # center image
+    x_ += offset_x
+    y_ += offset_y
+
+    # clip to values in image range
+    x = np.clip(x_, 0, w - 1).round().astype(int)
+    y = np.clip(y_, 0, h - 1).round().astype(int)
+
+    # filter out invalid coordinates (out of bounds)
+    filter_invalid_coordinates = np.logical_or(
+        np.logical_or(x_ > w - 1, y_ > h - 1),
+        np.logical_or(x_ < 0, y_ < 0)
+    )
+
+    # calculate dx and dy
+    indexes[0] = x
+    indexes[1] = y
+
+    if method == 4:
+        rotated_img = interpolate_lagrange(indexes, original_img)
+    elif method == 3:
+        rotated_img = interpolate_bicubic(indexes, original_img)
+    elif method == 2:
+        rotated_img = interpolate_bilinear(indexes, original_img)
+    else:
+        rotated_img = interpolate_nn(indexes, original_img)
+
+    # paint areas out of image range as black
+    rotated_img[filter_invalid_coordinates] = 0
+    return rotated_img
+
+
+def resize_image(method, original_img, width, height):
+    scale_x = width / original_img.shape[0]
+    scale_y = height / original_img.shape[1]
+
+    # scale
+    indexes = np.indices((width, height))
+    indexes[0] = indexes[0] / scale_x
+    indexes[1] = indexes[1] / scale_y
+
+    if method == 4:
+        return interpolate_lagrange(indexes, original_img)
+    elif method == 3:
+        return interpolate_bicubic(indexes, original_img)
+    elif method == 2:
+        return interpolate_bilinear(indexes, original_img)
+    else:
+        return interpolate_nn(indexes, original_img)
+
+    return None
+
+
+def main():
+    # files = [
+    #     'baboon.png',
+    #     'butterfly.png',
+    #     'city.png',
+    #     'house.png',
+    #     'seagull.png',
+    # ]
+    # for i in range(1,5):
+    #     for f in files:
+    #         i = str(i)
+    #         print('python3 main.py -m ' + i + ' -a 27 -i ' + f + ' -o rotated' + i + '.' + f)
+    #         print('python3 main.py -m ' + i + ' -e 2.63 -i ' + f + ' -o scaled' + i + '.' + f)
+    #         print('python3 main.py -m ' + i + ' -d 1000 500 -i ' + f + ' -o resized' + i + '.' + f)
+
+    parser = argparse.ArgumentParser(description='Scale and Rotate image.')
+    parser.add_argument('-a', type=float, help='angle')
+    parser.add_argument('-e', type=float, help='scale')
+    parser.add_argument('-d', type=int, help='width and height', nargs=2)
+    parser.add_argument('-m', type=int, help='interpolation method', choices=range(1, 5))
+    parser.add_argument('-i', help='input image')
+    parser.add_argument('-o', help='output image')
+
+    args = parser.parse_args()
+
+    input_img_file = args.i
+    output_img_file = args.o
+    method = args.m
+    angle = args.a
+    scale = args.e
+    dimensions = args.d
+
+    # read image
+    original_img = misc.imread(input_img_file)
+
+    # apply transformation
+    if angle:
+        output_img = rotate_image(method, original_img, angle)
+    elif scale:
+        output_img = scale_image(method, original_img, scale)
+    elif dimensions:
+        width = dimensions[0]
+        height = dimensions[1]
+        output_img = resize_image(method, original_img, width, height)
+
+    # save output image
+    io.imsave(output_img_file, output_img, cmap='gray', vmin=0, vmax=255)
+
+if __name__ == "__main__":
+    main()
+
---
+++ ex4/run.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#
+# Usage: python3 main.py -m <method> -a <angle> -e <scale> -d <width> <height> -i <input image> -o <output image>
+#
+
+python3 main.py -m 1 -a 27 -i baboon.png -o rotated1.baboon.png
+python3 main.py -m 1 -e 2.63 -i baboon.png -o scaled1.baboon.png
+python3 main.py -m 1 -d 1000 500 -i baboon.png -o resized1.baboon.png
+python3 main.py -m 1 -a 27 -i butterfly.png -o rotated1.butterfly.png
+python3 main.py -m 1 -e 2.63 -i butterfly.png -o scaled1.butterfly.png
+python3 main.py -m 1 -d 1000 500 -i butterfly.png -o resized1.butterfly.png
+python3 main.py -m 1 -a 27 -i city.png -o rotated1.city.png
+python3 main.py -m 1 -e 2.63 -i city.png -o scaled1.city.png
+python3 main.py -m 1 -d 1000 500 -i city.png -o resized1.city.png
+python3 main.py -m 1 -a 27 -i house.png -o rotated1.house.png
+python3 main.py -m 1 -e 2.63 -i house.png -o scaled1.house.png
+python3 main.py -m 1 -d 1000 500 -i house.png -o resized1.house.png
+python3 main.py -m 1 -a 27 -i seagull.png -o rotated1.seagull.png
+python3 main.py -m 1 -e 2.63 -i seagull.png -o scaled1.seagull.png
+python3 main.py -m 1 -d 1000 500 -i seagull.png -o resized1.seagull.png
+python3 main.py -m 2 -a 27 -i baboon.png -o rotated2.baboon.png
+python3 main.py -m 2 -e 2.63 -i baboon.png -o scaled2.baboon.png
+python3 main.py -m 2 -d 1000 500 -i baboon.png -o resized2.baboon.png
+python3 main.py -m 2 -a 27 -i butterfly.png -o rotated2.butterfly.png
+python3 main.py -m 2 -e 2.63 -i butterfly.png -o scaled2.butterfly.png
+python3 main.py -m 2 -d 1000 500 -i butterfly.png -o resized2.butterfly.png
+python3 main.py -m 2 -a 27 -i city.png -o rotated2.city.png
+python3 main.py -m 2 -e 2.63 -i city.png -o scaled2.city.png
+python3 main.py -m 2 -d 1000 500 -i city.png -o resized2.city.png
+python3 main.py -m 2 -a 27 -i house.png -o rotated2.house.png
+python3 main.py -m 2 -e 2.63 -i house.png -o scaled2.house.png
+python3 main.py -m 2 -d 1000 500 -i house.png -o resized2.house.png
+python3 main.py -m 2 -a 27 -i seagull.png -o rotated2.seagull.png
+python3 main.py -m 2 -e 2.63 -i seagull.png -o scaled2.seagull.png
+python3 main.py -m 2 -d 1000 500 -i seagull.png -o resized2.seagull.png
+python3 main.py -m 3 -a 27 -i baboon.png -o rotated3.baboon.png
+python3 main.py -m 3 -e 2.63 -i baboon.png -o scaled3.baboon.png
+python3 main.py -m 3 -d 1000 500 -i baboon.png -o resized3.baboon.png
+python3 main.py -m 3 -a 27 -i butterfly.png -o rotated3.butterfly.png
+python3 main.py -m 3 -e 2.63 -i butterfly.png -o scaled3.butterfly.png
+python3 main.py -m 3 -d 1000 500 -i butterfly.png -o resized3.butterfly.png
+python3 main.py -m 3 -a 27 -i city.png -o rotated3.city.png
+python3 main.py -m 3 -e 2.63 -i city.png -o scaled3.city.png
+python3 main.py -m 3 -d 1000 500 -i city.png -o resized3.city.png
+python3 main.py -m 3 -a 27 -i house.png -o rotated3.house.png
+python3 main.py -m 3 -e 2.63 -i house.png -o scaled3.house.png
+python3 main.py -m 3 -d 1000 500 -i house.png -o resized3.house.png
+python3 main.py -m 3 -a 27 -i seagull.png -o rotated3.seagull.png
+python3 main.py -m 3 -e 2.63 -i seagull.png -o scaled3.seagull.png
+python3 main.py -m 3 -d 1000 500 -i seagull.png -o resized3.seagull.png
+python3 main.py -m 4 -a 27 -i baboon.png -o rotated4.baboon.png
+python3 main.py -m 4 -e 2.63 -i baboon.png -o scaled4.baboon.png
+python3 main.py -m 4 -d 1000 500 -i baboon.png -o resized4.baboon.png
+python3 main.py -m 4 -a 27 -i butterfly.png -o rotated4.butterfly.png
+python3 main.py -m 4 -e 2.63 -i butterfly.png -o scaled4.butterfly.png
+python3 main.py -m 4 -d 1000 500 -i butterfly.png -o resized4.butterfly.png
+python3 main.py -m 4 -a 27 -i city.png -o rotated4.city.png
+python3 main.py -m 4 -e 2.63 -i city.png -o scaled4.city.png
+python3 main.py -m 4 -d 1000 500 -i city.png -o resized4.city.png
+python3 main.py -m 4 -a 27 -i house.png -o rotated4.house.png
+python3 main.py -m 4 -e 2.63 -i house.png -o scaled4.house.png
+python3 main.py -m 4 -d 1000 500 -i house.png -o resized4.house.png
+python3 main.py -m 4 -a 27 -i seagull.png -o rotated4.seagull.png
+python3 main.py -m 4 -e 2.63 -i seagull.png -o scaled4.seagull.png
+python3 main.py -m 4 -d 1000 500 -i seagull.png -o resized4.seagull.png
+
